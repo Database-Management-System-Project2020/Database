486af68668bd5dc0e0885e5d9eb6503ad7a06ec2
toqa <toqa7elshahat@gmail.com>
2020-04-30 02:03:27 +0200

    cute tables

---
+++ books.php
@@ -0,0 +1,321 @@
+<?php
+
+$conn;
+function connect(){
+$servername = "localhost";
+$username = "root";
+$dbname = "mydb";
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     echo "Connected successfully"; 
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$GLOBALS["conn" ]=$conn;
+}
+
+class Books
+{
+	public $product_product_id;
+	public $product_stock_idstock;
+    public $brand_name;
+    public $parts;
+    public $educational_stage;
+    public $subject;
+    
+
+
+
+	
+	function __construct($brand_name,$parts, $educational_stage, $subject)
+	{
+
+		function getMAXproduct(){
+    	try {
+    		connect();
+        	$stmt = $GLOBALS["conn" ]->prepare("SELECT product_id from product ORDER BY product_id DESC LIMIT 0, 1");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+		}
+		function getMAXstock(){
+    	try {
+    		connect();
+        	$stmt = $GLOBALS["conn" ]->prepare("SELECT stock_idstock from product ORDER BY stock_idstock DESC LIMIT 0, 1");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+		}
+		$product_stock_idstock= getMAXstock();
+		$product_product_id = getMAXproduct();
+		$this ->ID = $product_product_id;
+		$this ->ID_stock =$product_stock_idstock;
+		$this->_brand_name = $brand_name;
+	    $this->_parts = $parts;
+	    $this->_educational_stage = $educational_stage;
+	    $this->_subject = $subject;
+	    $stmt = $GLOBALS["conn" ]->prepare("INSERT INTO books (product_product_id,product_stock_idstock,brand_name, parts, educational_stage, subject) VALUES (:product_product_id,:product_stock_idstock,:brand_name,:parts,:educational_stage,:subject)");
+	    $stmt->bindParam(':product_product_id', $product_product_id);
+	    $stmt->bindParam(':product_stock_idstock', $product_stock_idstock);
+    	$stmt->bindParam(':brand_name', $brand_name);
+    	$stmt->bindParam(':parts', $parts);
+    	$stmt->bindParam(':educational_stage', $educational_stage);
+    	$stmt->bindParam(':subject', $subject);
+    	$stmt->execute();
+    	echo "innnssseeerreeedddd";
+
+
+
+
+	}
+	static function get_brand_name($ID)
+	{
+		try {
+			connect();
+        	$stmt = $GLOBALS["conn"]->prepare("SELECT brand_name FROM books WHERE $ID = product_product_id");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+		    }
+
+    static function get_parts($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT parts FROM books WHERE $ID = product_product_id");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+
+    static function get_educational_stage($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT educational_stage FROM books WHERE $ID = product_product_id ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+    static function get_subject($ID)
+	{	try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT subject FROM books WHERE $ID = product_product_id ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+        static function get_price ($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT product_price FROM product left outer JOIN  books ON product.product_id = books.product_product_id
+		ORDER BY  books.product_product_id ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+    static function get_barcode($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT product_barcode FROM product left outer JOIN  books ON product.product_id = books.product_product_id
+		ORDER BY  books.product_product_id");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+    static function get_amount_available($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT amount_available FROM product left outer JOIN  books ON product.product_id = books.product_product_id
+		ORDER BY  books.product_product_id ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+		
+    }
+    static function get_description($ID)
+	{
+		try {
+			connect();
+			$stmt = $GLOBALS["conn"]->prepare("SELECT product_description FROM product left outer JOIN  books ON product.product_id = books.product_product_id
+		ORDER BY  books.product_product_id ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+    }
+
+//_____________________________________________________________________________
+    //######UPDATE FUNCTIONS#############
+//_____________________________________________________________________________
+
+
+
+
+
+
+    static function update_brand_name($brand_name, $ID)
+	{
+		connect();
+		
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `books` SET `brand_name` = :brand_name  WHERE `product_product_id`=:product_product_id ");
+		$stmt->bindParam(':brand_name', $brand_name);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+    	
+    }
+    static function update_parts($parts,$ID)
+	{
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `books` SET `parts` = :parts  WHERE `product_product_id`= :product_product_id ");
+		$stmt->bindParam(':parts', $parts);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+    }
+     function update_subject($subject,$ID)
+	{
+		connect();
+
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `books` SET `subject` = :subject  WHERE `product_product_id`= :product_product_id ");
+		$stmt->bindParam(':subject', $subject);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+    }
+     function update_educational_stage($educational_stage,$ID)
+	{
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `books` SET `educational_stage` = :educational_stage  WHERE `product_product_id`= :product_product_id ");
+		$stmt->bindParam(':educational_stage', $educational_stage);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+		
+    }
+
+    function update_price($price,$ID)
+	{
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `product` SET `product_price` = :price  WHERE `product_id`= :product_product_id ");
+		$stmt->bindParam(':price', $price);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+		
+    }
+   
+    function update_available_amount($available_amount, $ID)
+	{
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `product` SET `available_amount` = :available_amount WHERE  `product_id`= :product_product_id");
+		$stmt->bindParam(':available_amount', $available_amount);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+
+		
+    }
+    function update_product_barcode($product_barcode, $ID)
+	{
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `product` SET `product_barcode` = :product_barcode WHERE  `product_id`= :product_product_id");
+		$stmt->bindParam(':product_barcode', $product_barcode);
+        $stmt->bindParam(':product_product_id', $ID);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+
+		
+    }
+
+
+
+
+}
+
+
+
+?>
---
+++ employee.php
@@ -0,0 +1,164 @@
+<?php
+$conn;
+function connect(){
+$servername = "localhost";
+$username = "root";
+$dbname = "mydb";
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     echo "Connected successfully"; 
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$GLOBALS["conn"]=$conn;
+}
+
+
+class employee{
+	public $idemployee;
+	public $name_emp;
+	public $job_description;
+	public $type;
+	public $employee_type_idemployee_type;
+
+	function getMAXid_employee_type(){
+    	try {
+    		connect();
+
+        	$stmt = $GLOBALS["conn"]->prepare("SELECT idemployee_type from employee_type ORDER BY idemployee_type DESC LIMIT 0, 1");
+    		$stmt->execute();
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+		}
+
+
+
+	static function insert_employee($name_emp, $job_description , $type ){
+		$employee_type_idemployee_type = employee::getMAXid_employee_type();
+		$stmt = $GLOBALS["conn"]->prepare("INSERT INTO employee (name_emp, job_description,type,employee_type_idemployee_type ) values (:name_emp,:job_description,:type,:employee_type_idemployee_type)");
+		$stmt->bindParam(':name_emp', $name_emp);
+	    $stmt->bindParam(':job_description', $job_description);
+    	$stmt->bindParam(':type', $type);
+    	$stmt->bindParam(':employee_type_idemployee_type', $employee_type_idemployee_type);
+    	$stmt->execute();
+
+	}
+	static function get_emp_name($idemployee){
+		connect();
+
+		try {
+			$stmt = $GLOBALS["conn"]->prepare("SELECT name_emp FROM employee WHERE idemployee = $idemployee");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+	}
+	static function get_job_description($idemployee){
+		connect();
+
+		try {
+			$stmt = $GLOBALS["conn"]->prepare("SELECT job_description FROM employee WHERE idemployee = $idemployee");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+	}
+	static function get_type($idemployee){
+		connect();
+
+		try {
+			$stmt = $GLOBALS["conn"]->prepare("SELECT type FROM employee WHERE idemployee = $idemployee");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+	}
+	static function get_idemployee_type($idemployee){
+		connect();
+
+		try {
+			
+			$stmt = $GLOBALS["conn"]->prepare("SELECT employee_type_idemployee_type FROM employee WHERE idemployee = $idemployee ");
+    		$stmt->execute();
+
+    		// set the resulting array to associative
+    		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    		return $stmt->fetchColumn();
+    		$GLOBALS["conn" ]=null;
+			}
+		catch(PDOException $e) {
+    		echo "Error: " . $e->getMessage();
+			}
+
+	}
+	static function update_name_emp ($name_emp ,$idemployee){
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `employee` SET `name_emp` = :name_emp  WHERE `idemployee`=:idemployee ");
+		$stmt->bindParam(':name_emp', $name_emp);
+        $stmt->bindParam(':idemployee', $idemployee);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+
+	}
+	static function update_type ($type ,$idemployee){
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `employee` SET `type` = :type  WHERE `idemployee`=:idemployee ");
+		$stmt->bindParam(':type', $type);
+        $stmt->bindParam(':idemployee', $idemployee);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+
+	}
+	static function update_job_description ($job_description ,$idemployee){
+		connect();
+		$stmt = $GLOBALS["conn"]->prepare("UPDATE `employee` SET `job_description` = :job_description  WHERE `idemployee`=:idemployee ");
+		$stmt->bindParam(':job_description', $job_description);
+        $stmt->bindParam(':idemployee', $idemployee);
+    	$stmt->execute();
+    	$GLOBALS["conn" ]=null;
+
+	}
+
+	
+
+
+
+
+
+
+}
+
+
+
+?>
